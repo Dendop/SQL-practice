32026ec0ae429ce2a1f9bd057fe48cc5bccf4f97
Dendop <denopod@gmail.com>
2025-04-30 13:21:12 +0100

    a

--- chicago_crimes/queries.sql
+++ chicago_crimes/queries.sql
@@ -38,14 +38,3 @@ GROUP BY "communities"."name", "communities"."population", "communities"."densit
 ORDER BY "number_of_crimes" ASC
 LIMIT 10;
 
---6. What month had the most crimes reported and what was the average and median temperature high in the last six years?
---7. What month had the most homicides reported and what was the average and median temperature high in the last six years?
---8. List the most violent year and the number of arrests with percentage. Order by the number of crimes in decending order. Determine the most violent year by the number of reported Homicides, Assaults and Battery for that year.
---9. List the day of the week, year, average precipitation, average high temperature and the highest number of reported crimes for days with and without precipitation.
---10. List the days with the most reported crimes when there is zero precipitation and the day when precipitation is greater than .5". Include the day of the week, high temperature, amount and precipitation and the total number of reported crimes for that day.
---11. List the most consecutive days where a homicide occured between 2018-2023 and the timeframe.
---12. What are the top 10 most common locations for reported crimes and the number of reported crime (add percentage) depending on the temperature?
---13. Calculate the year over year growth in the number of reported crimes.
---14. Calculate the year over year growth in the number of reported domestic violence crimes.
---15. Calculate the year over year growth in the number of reported domestic violence crimes.
---16. List the number of crimes reported and seasonal growth for each astronomical season and what was the average temperature for each season in 2020? Use a conditional statement to display either a Gain/Loss for the season and the season over season growth.
